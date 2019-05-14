974339756ab7d1bd8eb6a6dff31962711ced8dd0
nabillawr <nabillawr15@gmail.com>
2019-05-14 12:13:40 +0700

    TugasPolymorphism

---
+++ TugasPolymorphism/LasetJet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasPolymorphism
+{
+    public class LasetJet : PrinterWindows
+    {
+        public override void Show()
+        {
+            Console.WriteLine("LaseJet display dimention : 12*12");
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("LaseJet printer printing...");
+        }
+    }
+}
